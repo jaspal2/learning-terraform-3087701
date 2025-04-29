d48078a3c6821be303dae900df314823dfb4899a
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-04-29 13:43:43 +1000

    adding vpc

--- main.tf
+++ main.tf
@@ -14,9 +14,45 @@ data "aws_ami" "app_ami" {
   owners = ["964913206263"] # AWS
 }
 
+
+resource "aws_vpc" "custom_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_security_group" "allow_http_https" {
+  name        = "allow_https/https"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name = "allow_http(s)"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws.aws_security_group.id]
 
   tags = {
     Name = "HelloWorld"
