d654e79c904f2d527051af94a5eeae7c74ac3f00
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-04-29 15:21:35 +1000

    asscoiate public ip

--- main.tf
+++ main.tf
@@ -113,6 +113,7 @@ resource "aws_route_table_association" "associate_route_to_subnet" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.allow_http_https.id]
    subnet_id             = aws_subnet.public.id
    key_name              = aws_key_pair.ssh_key.id
