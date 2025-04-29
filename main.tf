ce4caa9c5dd883e635d2049989f8df07c83daa15
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-04-29 13:47:22 +1000

    fized vpc file changes

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_vpc" "custom_vpc" {
 resource "aws_security_group" "allow_http_https" {
   name        = "allow_https/https"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.vpc.id
+  vpc_id      = aws_vpc.custom_vpc.id
 
   tags = {
     Name = "allow_http(s)"
@@ -35,7 +35,7 @@ resource "aws_security_group" "allow_http_https" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.allow_http_https.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
@@ -52,7 +52,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws.aws_security_group.id]
+  vpc_security_group_ids = [aws_security_group.allow_http_https.id]
 
   tags = {
     Name = "HelloWorld"
