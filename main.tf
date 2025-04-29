2b238edad9a510c00ad4cc57af9f385b3fde4fe8
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-04-29 13:49:45 +1000

    fixed error

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.allow_http_https.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
